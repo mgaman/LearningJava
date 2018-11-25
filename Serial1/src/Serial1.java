7c412bdc65b82fb534cfc0491defe06e4af4a9e5
David Henry <mgadriver@gmail.com>
2018-11-25 16:50:32 +0200

    Added serial event to Serial1

--- Serial1/src/Serial1.java
+++ Serial1/src/Serial1.java
@@ -6,27 +6,33 @@ public class Serial1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SerialPort [] ports = SerialPort.getCommPorts();
+		int foundPort = -1;
 		for (int i=0;i<ports.length;i++)
 		{
 			System.out.println(ports[i].getSystemPortName());
-			System.out.println(ports[i].getDescriptivePortName());
-			System.out.println(ports[i].getPortDescription());
+//			System.out.println(ports[i].getDescriptivePortName());
+//			System.out.println(ports[i].getPortDescription());
+			if (ports[i].getSystemPortName().equals(args[0]))
+				foundPort = i;
 		}
-		SerialPort comPort = SerialPort.getCommPorts()[0];
-		comPort.openPort();
-		try {
-		   while (true)
-		   {
-		      while (comPort.bytesAvailable() <= 0)
-		         Thread.sleep(20);
-
-		      byte[] readBuffer = new byte[comPort.bytesAvailable()];
-		      int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-		      System.out.println(new String(readBuffer));
-//		      System.out.println("Read " + numRead + " bytes.");
-		   }
-		} catch (Exception e) { e.printStackTrace(); }
-		System.out.println("Closing");
-		comPort.closePort();
+		SerialPort comport = ports[foundPort];
+		comport.openPort();
+		comport.addDataListener(new SerialPortDataListener() {
+			@Override
+			   public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+			   @Override
+			   public void serialEvent(SerialPortEvent event)
+			   {
+			      if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+			         return;
+			      byte[] newData = new byte[comport.bytesAvailable()];
+			      int numRead = comport.readBytes(newData, newData.length);
+			      System.out.print(new String(newData));
+			    //  System.out.println("Read " + numRead + " bytes.");
+			   }	
+		});
+		while (true) {}
+//		System.out.println("Closing");
+	//	ports[foundPort].closePort();
 	}
 }
