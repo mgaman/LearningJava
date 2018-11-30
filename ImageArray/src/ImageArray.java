4d0ba9c73554ef79c95ba4ee9145b55c164c3378
david <david@10.0.0.14>
2018-11-30 10:58:07 +0200

    first go

---
+++ ImageArray/src/ImageArray.java
@@ -0,0 +1,64 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class ImageArray {
+
+	protected Shell shlImagearray;
+	protected int index = 0;
+//	protected String basepath = "C:\\Users\\david\\Downloads\\images\\";
+	protected String basepath = "Images/";
+	/* Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			ImageArray window = new ImageArray();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shlImagearray.open();
+		shlImagearray.layout();
+		while (!shlImagearray.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlImagearray = new Shell();
+		shlImagearray.setSize(142, 164);
+		shlImagearray.setText("ImageArray");
+		
+		Button btnNewButton = new Button(shlImagearray, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String path = String.format(basepath + "signal-%d-bar.png", index);
+				btnNewButton.setImage(SWTResourceManager.getImage(path));
+				index++;
+				index = index % 6;
+			}
+		});
+		String path = String.format(basepath + "signal-%d-bar.png", index);
+		btnNewButton.setImage(SWTResourceManager.getImage(path));
+		btnNewButton.setBounds(10, 33, 86, 66);
+	}
+}
